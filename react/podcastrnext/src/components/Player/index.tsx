8cd682b0e364bd85fadf35f6df8e3c365b76087f
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-04-23 12:19:35 -0400

    Finish project

--- react/podcastrnext/src/components/Player/index.tsx
+++ react/podcastrnext/src/components/Player/index.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
 
 import styles from './styles.module.scss'
 import { usePlayer } from '../../contexts/PlayerContexts'
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString'
 
 export function Player() {
   const audioRef = useRef<HTMLAudioElement>(null)
+  const [progress, setProgress] = useState(0)
 
   const {
     episodeList,
@@ -15,10 +17,15 @@ export function Player() {
     playPrevious,
     isPlaying,
     playNext,
+    isLooping,
+    toggleLoop,
     togglePlay,
+    isShuffling,
+    toggleShuffle,
     setPlayingState,
     hasNext,
-    hasPrevious
+    hasPrevious,
+    clearPlayerState
   } = usePlayer()
 
   const episode = episodeList[currentEpisodeIndex]
@@ -34,6 +41,27 @@ export function Player() {
       audioRef.current.pause()
     }
   }, [isPlaying])
+
+  function setupProgressListener() {
+    audioRef.current.currentTime = 0
+
+    audioRef.current.addEventListener('timeupdate',() => {
+      setProgress(Math.floor(audioRef.current.currentTime))
+    })
+  }
+
+  function handleSeek(amount: number) {
+    audioRef.current.currentTime = amount
+    setProgress(amount)
+  }
+
+  function handleEpisodeEnded() {
+    if(hasNext) {
+      playNext()
+    } else {
+      clearPlayerState()
+    }
+  }
 
   return (
     <div className={styles.playerContainer}>
@@ -62,10 +90,13 @@ export function Player() {
 
       <footer className={!episode ? styles.empty : ''}>
         <div className={styles.progress}>
-          <span>00:00</span>
+        <span>{convertDurationToTimeString(progress)}</span>
           <div className={styles.slider}>
             {episode ? (
               <Slider
+                max={episode.duration}
+                value={progress}
+                onChange={handleSeek}
                 trackStyle={{ backgroundColor: '#04d361'}}
                 railStyle={{ backgroundColor: '#9f75ff'}}
                 handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
@@ -74,7 +105,7 @@ export function Player() {
               <div className={styles.emptySlider} />
             )}
           </div>
-          <span>00:00</span>
+          <span>{convertDurationToTimeString(episode?.duration ?? 0)}</span>
         </div>
 
         {episode && (
@@ -82,13 +113,21 @@ export function Player() {
             ref={audioRef}
             src={episode.url}
             autoPlay
+            onEnded={handleEpisodeEnded}
+            loop={isLooping}
             onPlay={() => setPlayingState(true)}
             onPause={() => setPlayingState(false)}
+            onLoadedMetadata={setupProgressListener}
           />
         )}
 
         <div className={styles.buttons}>
-          <button type="button" disabled={!episode}>
+          <button
+            type="button"
+            disabled={!episode || episodeList.length === 1}
+            onClick={toggleShuffle}
+            className={isShuffling ? styles.isActive : ''}
+          >
             <img src="/shuffle.svg" alt="Embaralhar"/>
           </button>
           <button type="button" disabled={!episode || !hasPrevious} onClick={playPrevious}>
@@ -108,7 +147,12 @@ export function Player() {
           <button type="button" disabled={!episode || !hasNext} onClick={playNext}>
             <img src="/play-next.svg" alt="Tocar próxima"/>
           </button>
-          <button type="button" disabled={!episode}>
+          <button
+            type="button"
+            disabled={!episode}
+            onClick={toggleLoop}
+            className={isLooping ? styles.isActive: ''}
+          >
             <img src="/repeat.svg" alt="Repetir"/>
           </button>
         </div>
--- react/podcastrnext/src/contexts/PlayerContexts.tsx
+++ react/podcastrnext/src/contexts/PlayerContexts.tsx
@@ -13,6 +13,10 @@ type PlayerContextData = {
   currentEpisodeIndex: number
   isPlaying: boolean
   playPrevious: () => void
+  isLooping: boolean
+  toggleLoop: () => void
+  isShuffling: boolean
+  toggleShuffle: () => void
   play: (episode: Episode) => void
   playNext: () => void
   playList: (list: Episode[], index: number) => void
@@ -20,6 +24,7 @@ type PlayerContextData = {
   setPlayingState: (state: boolean) => void
   hasNext: boolean,
   hasPrevious: boolean
+  clearPlayerState: () => void
 }
 
 const PlayerContext = createContext({} as PlayerContextData)
@@ -32,6 +37,8 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
   const [episodeList, setEpisodeList] = useState([])
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
   const [isPlaying, setIsPlaying] = useState(false)
+  const [isLooping, setIsLooping] = useState(false)
+  const [isShuffling, setIsShuffling] = useState(false)
   
   function play(episode: Episode) {
     setEpisodeList([episode])
@@ -49,15 +56,26 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
     setIsPlaying(!isPlaying)
   }
 
+  function toggleLoop() {
+    setIsLooping(!isLooping)
+  }
+
+  function toggleShuffle() {
+    setIsShuffling(!isShuffling)
+  }
+
   function setPlayingState(state: boolean) {
     setIsPlaying(state)
   }
 
   const hasPrevious = currentEpisodeIndex > 0
-  const hasNext = (currentEpisodeIndex + 1) < episodeList.length
+  const hasNext = isShuffling || (currentEpisodeIndex + 1) < episodeList.length
 
   function playNext() {
-    if (hasNext) {
+    if(isShuffling) {
+      const nextRandomEpisodeIndex = Math.floor((Math.random() * episodeList.length))
+      setCurrentEpisodeIndex(nextRandomEpisodeIndex)
+    } else if (hasNext) {
       setCurrentEpisodeIndex(currentEpisodeIndex + 1)
     }
   }
@@ -66,6 +84,11 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
     if (hasPrevious) {
       setCurrentEpisodeIndex(currentEpisodeIndex - 1)
     }
+  }
+
+  function clearPlayerState() {
+    setEpisodeList([])
+    setCurrentEpisodeIndex(0)
   }
 
   return (
@@ -80,9 +103,14 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
           playList,
           isPlaying,
           togglePlay,
+          isShuffling,
+          toggleShuffle,
+          isLooping,
+          toggleLoop,
           setPlayingState,
           hasNext,
-          hasPrevious
+          hasPrevious,
+          clearPlayerState
         }
       }
     >
