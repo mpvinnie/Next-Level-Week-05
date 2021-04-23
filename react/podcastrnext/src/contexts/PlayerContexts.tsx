7a6f7975d028e09fe7574347254e34eaf2585ce9
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-04-23 10:43:24 -0400

    Refactoring player context

--- react/podcastrnext/src/contexts/PlayerContexts.tsx
+++ react/podcastrnext/src/contexts/PlayerContexts.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react'
+import { createContext, ReactNode, useState } from 'react'
 
 type Episode = {
   title: string
@@ -18,3 +18,43 @@ type PlayerContextData = {
 }
 
 export const PlayerContext = createContext({} as PlayerContextData)
+
+type PlayerContextProviderProps = {
+  children: ReactNode
+}
+
+export function PlayerContextProvider({ children }: PlayerContextProviderProps) {
+  const [episodeList, setEpisodeList] = useState([])
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+  
+  function play(episode: Episode) {
+    setEpisodeList([episode])
+    setCurrentEpisodeIndex(0)
+    setIsPlaying(true)
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying)
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state)
+  }
+
+  return (
+    <PlayerContext.Provider
+      value={
+        {
+          episodeList,
+          currentEpisodeIndex,
+          play, isPlaying,
+          togglePlay,
+          setPlayingState
+        }
+      }
+    >
+      {children}
+    </PlayerContext.Provider>
+  )
+}
